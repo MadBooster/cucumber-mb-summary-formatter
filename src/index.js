9bf782f1315eb41c9fadd64973b2cda9c3377ec3
Tomi Nokkala <tomi.nokkala@madbooster.com>
2021-05-06 15:18:30 +0300

    Fix support for cucumber 7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import _ from 'lodash'
-import { formatSummary, isFailure, isWarning } from 'cucumber/lib/formatter/helpers'
-import { formatIssue } from './issue_helpers'
-import Formatter from 'cucumber/lib/formatter/'
-import Status from 'cucumber/lib/status'
+import SummaryFormatter from '@cucumber/cucumber/lib/formatter/summary_formatter'
+import { isFailure } from '@cucumber/cucumber/lib/formatter/helpers/issue_helpers'
+import Status from '@cucumber/cucumber/lib/status'
 
 const STATUS_CHARACTER_MAPPING = {
   [Status.AMBIGUOUS]: 'A',
@@ -10,94 +8,70 @@ const STATUS_CHARACTER_MAPPING = {
   [Status.PASSED]: '.',
   [Status.PENDING]: 'P',
   [Status.SKIPPED]: '-',
-  [Status.UNDEFINED]: 'U',
+  [Status.UNDEFINED]: 'U'
 }
 
-export default class SummaryFormatter extends Formatter {
+export default class MbSummaryFormatter extends SummaryFormatter {
   constructor(options) {
-    options.eventBroadcaster.on('test-run-finished', () => {
-      this.log('\n\n')
-    })
     super(options)
-    options.eventBroadcaster.on('pickle-accepted', this.incrementStepCount)
-    options.eventBroadcaster.on('test-step-finished', this.logProgress)
-    options.eventBroadcaster.on('test-case-finished', this.logStepProgress)
-    options.eventBroadcaster.on('test-run-finished', this.logSummary)
+    options.eventBroadcaster.on('envelope', this.parseEnvelope)
     this.numberOfSteps = 0
     this.numberOfScenarios = 0
     this.currentScenarioCount = 1
+    this.failureCount = 0
   }
 
-  incrementStepCount = ({ pickle, ...rest }) => {
+  parseEnvelope = envelope => {
+    if(envelope.pickle != null) {
+      this.incrementStepCount(envelope.pickle)
+    } else if(envelope.testStepFinished != null) {
+      this.logProgress(envelope.testStepFinished)
+    } else if(envelope.testCaseFinished != null) {
+      this.logStepProgress(envelope.testCaseFinished)
+    } else if(envelope.testRunFinished != null) {
+      this.log('\n\n')
+    }
+  }
+
+  incrementStepCount = (pickle) => {
     this.numberOfSteps += pickle.steps.length
     this.numberOfScenarios++
   }
 
-  logStepProgress = (data) => {
-    const { attemptNumber, result } = data
-    this.log(`${this.currentScenarioCount}/${this.numberOfScenarios} ${attemptNumber > 1 ? `(try ${attemptNumber})` : ''}\n`)
-    if(!(result.status === Status.FAILED && result.retried)) {
+  logStepProgress = (testCaseFinished) => {
+    const testCaseAttempt = this.eventDataCollector.getTestCaseAttempt(testCaseFinished.testCaseStartedId)
+    const { attempt, worstTestStepResult } = testCaseAttempt
+    if(worstTestStepResult.status === Status.FAILED && !worstTestStepResult.willBeRetried) {
+      this.failureCount++
+    }
+    this.log(`${this.currentScenarioCount}/${this.numberOfScenarios} ${attempt > 0 ? `(try ${attempt + 1})` : ''} ${this.failureCount > 0 ? this.colorFns.forStatus(Status.FAILED)(`(failed: ${this.failureCount})`) : ''}\n`)
+    if(!worstTestStepResult.willBeRetried) {
       this.currentScenarioCount++
     }
   }
 
-  logProgress = (data) => {
-    const {index, result, testCase} = data
-    const { status } = result
-    const character = this.colorFns[status](STATUS_CHARACTER_MAPPING[status])
+  logProgress = (progress) => {
+    const { testStepResult, testCaseStartedId, testStepId } = progress
+    const { status } = testStepResult
+    const character = this.colorFns.forStatus(status)(STATUS_CHARACTER_MAPPING[status])
     this.log(character)
-    if(isFailure(data)) {
-      const {
-        gherkinDocument,
-        pickle,
-        testCase: failureCase
-      } = this.eventDataCollector.getTestCaseAttempt(testCase)
-      const failureStep = failureCase.steps[index]
-      if(failureStep && failureStep.sourceLocation) {
-        this.log(this.colorFns[status](`\nFeature: ${pickle.name} / Step: ${failureStep.sourceLocation.uri}:${failureStep.sourceLocation.line})\n`))
-      } else {
-        this.log(this.colorFns[status](`\nBefore/after hook @ scenario: ${pickle.name} (${testCase.sourceLocation.uri}:${testCase.sourceLocation.line})\n`))
+    if(isFailure(testStepResult)) {
+      try {
+        const testCaseAttempt = this.eventDataCollector.getTestCaseAttempt(testCaseStartedId)
+        const failureStep = testCaseAttempt.testCase.testSteps.find(step => step.id === testStepId)
+        const failureStepData = testCaseAttempt.pickle.steps.find(step => step.id === failureStep.pickleStepId)
+        const failurePickleAstNodeId = testCaseAttempt.pickle.astNodeIds[0]
+        const failureScenario = testCaseAttempt.gherkinDocument.feature.children.find(scenario => (scenario.scenario ? scenario.scenario.id : scenario.background.id) === failurePickleAstNodeId)
+        if(failureStep && failureStep.pickleStepId) {
+          const failureStepAstData = failureScenario.scenario.steps.find(step => step.id === failureStepData.astNodeIds[0])
+          this.log(this.colorFns.forStatus(status)(`\nFeature: ${testCaseAttempt.pickle.name} / Step: "${failureStepData.text}" (${testCaseAttempt.pickle.uri}:${failureStepAstData.location.line})\n`))
+        } else {
+          const failureScenarioAstData = (failureScenario.scenario ? failureScenario.scenario : failureScenario.background)
+          this.log(this.colorFns.forStatus(status)(`\nBefore/after hook @ scenario: ${testCaseAttempt.pickle.name} (${testCaseAttempt.pickle.uri}:${failureScenarioAstData.location.line}))\n`))
+        }
+      } catch(e) {
+        console.error('Error on formatter', e)
       }
     }
-  }
-
-  logSummary = (testRun) => {
-    const failures = []
-    const warnings = []
-    const testCaseAttempts = this.eventDataCollector.getTestCaseAttempts()
-    _.each(testCaseAttempts, testCaseAttempt => {
-      if (isFailure(testCaseAttempt.result)) {
-        failures.push(testCaseAttempt)
-      } else if (isWarning(testCaseAttempt.result)) {
-        warnings.push(testCaseAttempt)
-      }
-    })
-    if (failures.length > 0) {
-      this.logIssues({ issues: failures, title: 'Failures' })
-    }
-    if (warnings.length > 0) {
-      this.logIssues({ issues: warnings, title: 'Warnings' })
-    }
-    this.log(
-      formatSummary({
-        colorFns: this.colorFns,
-        testCaseAttempts,
-        testRun,
-      })
-    )
-  }
-
-  logIssues = ({ issues, title }) => {
-    this.log(`${title}:\n\n`)
-    issues.forEach((testCaseAttempt, index) => {
-      this.log(
-        formatIssue({
-          colorFns: this.colorFns,
-          number: index + 1,
-          snippetBuilder: this.snippetBuilder,
-          testCaseAttempt,
-        })
-      )
-    })
   }
 }
